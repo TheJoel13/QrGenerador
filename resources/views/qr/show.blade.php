f5863a21548e5d9ca0997ebc64aae87520dbe90c
TheJoel13 <joelbalderramar13@gmail.com>
2025-08-27 23:53:52 -0600

    si

--- resources/views/qr/show.blade.php
+++ resources/views/qr/show.blade.php
@@ -5,8 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Incluye tu archivo CSS responsive -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
