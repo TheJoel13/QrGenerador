1e1c73203f974401d954a86dea800cccb9fe5959
TheJoel13 <joelbalderramar13@gmail.com>
2025-08-28 00:24:02 -0600

    Sds

--- resources/views/qr/show.blade.php
+++ resources/views/qr/show.blade.php
@@ -5,9 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    {{--     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     <title>QR Generated</title>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7412523202147499"
         crossorigin="anonymous"></script>
